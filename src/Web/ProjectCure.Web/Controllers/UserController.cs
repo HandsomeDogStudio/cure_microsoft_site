9cedd7d020e99864e6973da9099efeab5883b878
brian avent <brian.avent@gmail.com>
2014-03-07 20:18:10 -0600

    user list

---
+++ src/Web/ProjectCure.Web/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectCure.Web.Code;
+using ProjectCure.Web.Models;
+using ProjectCureData;
+
+namespace ProjectCure.Web.Controllers
+{
+    [CustomAuthorize(Roles = "Admin")]
+    public class UserController : ProjectCureControllerBase
+    {
+        public UserController(IRepository repository) : base(repository)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            return View(
+                new UserListModel
+                {
+                    Users = Repository.GetUserList()
+                });
+        }
+    }
+}
