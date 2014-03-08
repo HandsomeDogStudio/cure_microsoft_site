b1c8b7e9034fd813dd25121171eab78211bb42cc
Louis Fischer <louisfischer@gmail.com>
2014-03-07 18:33:04 -0600

    Incorporating Index into Layout

---
+++ src/Web/ProjectCure.Web/App_Common/StyleBundles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectCure.Web
+{
+    public static class StyleBundles
+    {
+        public const string Site = "~/Content/css/styles";
+    }
+}
--- src/Web/ProjectCure.Web/Controllers/AccountController.cs
+++ src/Web/ProjectCure.Web/Controllers/AccountController.cs
@@ -1,15 +1,12 @@
-﻿using System;
+﻿using ProjectCure.Web.Filters;
+using ProjectCure.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Transactions;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
-using ProjectCure.Web.Filters;
-using ProjectCure.Web.Models;
 
 namespace ProjectCure.Web.Controllers
 {
