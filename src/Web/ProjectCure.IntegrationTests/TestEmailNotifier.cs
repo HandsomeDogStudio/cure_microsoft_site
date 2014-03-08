d9c2fd4798b57aa22f20ad44da4a6a9e395546c0
Nick <nickolas.wood@hotmail.com>
2014-03-07 19:24:28 -0600

    Adding SMTP and Email Notifier

---
+++ src/Web/ProjectCure.IntegrationTests/TestEmailNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectCure.Web.Controllers;
+
+namespace ProjectCure.IntegrationTests
+{
+    [TestClass]
+    public class TestEmailNotifier
+    {
+        [TestMethod]
+        public void TestHappyPathEmailNotifier()
+        {
+            var testEmailer = new EmailNotifier();
+            testEmailer.SendNotification("nickolas.wood@hotmail.com", "Sign Up Template");
+        }
+    }
+}
